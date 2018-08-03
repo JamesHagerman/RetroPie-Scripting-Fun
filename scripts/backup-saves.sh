60b718506c6f38407588c6c4c81a78c2ed0d41e0
James Hagerman on CircuitSword <james.hagerman@gmail.com>
2018-08-03 11:25:41 -0700

    Adding a saved game backup helper script.

---
+++ scripts/backup-saves.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Note: This script uses the tree command. Install it with `sudo apt get install tree`
+
+echo "Putting all found game saves into ~/saves_backup/"
+
+pushd ~pi/ > /dev/null
+find ./RetroPie/roms/ -iname "*.srm" -exec rsync -aR {} ~/saves_backup/ \;
+popd > /dev/null
+
+tree ~/saves_backup/
+
+echo "Create an archive of all saves using: 'cd ~pi & tar -zcvf saves.tar.gz ./saves_backup/*'"
+
